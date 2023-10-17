3d883af3278847c69740b756e3d00ad3ca329cc6
Moni Waterhouse <monica.waterhouse@estudiantec.cr>
2023-10-17 11:37:31 -0600

    Create GUI with pygame menu to enter flight parameyters

--- client/config.py
+++ client/config.py
@@ -2,6 +2,11 @@ from cflib.utils import uri_helper
 class Config:
 
     BASE_URL = 'http://127.0.0.1:5000'
-    SIDE_DISTANCE = 0.2
+
+    TRAVEL_DISTANCE = 0.2
     TARGET_ALT = 0.5
     PHEROMONE_INTENSITY = 500
+    SPEED = 0.2
+    DRONE_NUMBER = 1
+
+    TERRITORY_PATH = "/Users/moniwaterhouse/TFG-ComputerEngineering-DPW/Territory/territory.txt"
